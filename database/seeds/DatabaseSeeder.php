0ca76972ddeb98dc58ee5ad72edde5a6bb09b090
DESKTOP-UPQ61CI\geria <geriadam1@gmail.com>
2019-01-31 23:23:29 +0700

    add vote and upvote sistem

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UsersQuestionsAnswersTableSeeder::class,
             FavoritesTableSeeder::class,
+            VotablesTableSeeder::class,
         ]);
     }
 }
